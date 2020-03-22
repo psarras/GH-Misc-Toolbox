9771be501a49f473ee5319512ca9b530667886f3
spsarras <psarras.st@gmail.com>
2020-03-22 11:41:43 +0000

    get the value stats of a list of values

---
+++ GH.MiscToolbox/GH.MiscToolbox/Components/Analytics/ValueStatsComponent.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace GH.MiscToolbox.Components
+{
+    public class ValueStatsComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the ValueStatsComponent class.
+        /// </summary>
+        public ValueStatsComponent()
+          : base("Value Stats", "ValStats",
+              "Get min, max, average value",
+              "MiscToolbox", "Analytics")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Value", "V", "Values to analyse", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Min", "m", "Min value", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Max", "M", "Max value", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Average", "A", "Average value", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var list = new List<double>();
+            if (!DA.GetDataList(0, list))
+                return;
+
+            DA.SetData(0, list.Min());
+            DA.SetData(1, list.Max());
+            DA.SetData(2, list.Average());
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("f9f846ce-12c7-4cd2-8869-51b28736d6d5"); }
+        }
+    }
+}

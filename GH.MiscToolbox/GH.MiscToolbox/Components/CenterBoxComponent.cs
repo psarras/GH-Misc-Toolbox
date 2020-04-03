68a728887727143afcb8ee6c1a0b0d8941c8efca
spsarras <psarras.st@gmail.com>
2020-04-03 21:13:07 +0100

    added a utility to recenter a BoundingBox

---
+++ GH.MiscToolbox/GH.MiscToolbox/Components/CenterBoxComponent.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace GH.MiscToolbox.Components
+{
+    public class CenterBoxComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the CenterBoxComponent class.
+        /// </summary>
+        public CenterBoxComponent()
+          : base("Center Box", "CenterBox",
+              "Description",
+              "MiscToolbox", "Utilities")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBoxParameter("Box", "B", "Bounding Box to Center", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBoxParameter("Box", "B", "Bounding Box to Center", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Box box = new Box();
+            if (!DA.GetData(0, ref box))
+                return;
+            
+            var x = box.X.Length / 2;
+            var y = box.Y.Length / 2;
+            var z = box.Z.Length / 2;
+
+            Box b = new Box(new Plane(box.Center, box.Plane.XAxis, box.Plane.YAxis), new Interval(-x, x), new Interval(-y, y), new Interval(-z, z));
+
+            DA.SetData(0, b);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("2ab703ed-4681-46c7-818d-dd492b3701c2"); }
+        }
+    }
+}

f1f0aa64f1ad597cb030dadae86e39c7018605bc
spsarras <psarras.st@gmail.com>
2020-04-10 00:00:57 +0100

    made the Mesh Ray component to work with multiple targets

--- GH.MiscToolbox/GH.MiscToolbox/Components/Analysis/MeshRayIntersectionsParallelComponent.cs
+++ GH.MiscToolbox/GH.MiscToolbox/Components/Analysis/MeshRayIntersectionsParallelComponent.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 
 namespace GH.MiscToolbox.Components
@@ -24,9 +26,9 @@ namespace GH.MiscToolbox.Components
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddMeshParameter("Mesh", "M", "Context mesh that can obstruct target", GH_ParamAccess.list);
-            pManager.AddMeshParameter("Target Mesh", "Mt", "Mesh for a target to check against", GH_ParamAccess.item); //List
+            pManager.AddMeshParameter("Target Mesh", "Mt", "Mesh for a target to check against", GH_ParamAccess.list); //List
             pManager.AddPointParameter("Point", "P", "Point to start ray from", GH_ParamAccess.list);
-            pManager.AddPointParameter("Target", "T", "Points representing the target", GH_ParamAccess.list); // Tree
+            pManager.AddPointParameter("Target", "T", "Points representing the target", GH_ParamAccess.tree); // Tree
             pManager.AddBooleanParameter("Run", "R", "Run analysis", GH_ParamAccess.item);
         }
 
@@ -35,7 +37,7 @@ namespace GH.MiscToolbox.Components
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("Intersections", "I", "percentage of target points each panel hits", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Intersections", "I", "percentage of target points each panel hits", GH_ParamAccess.tree);
             pManager.AddLineParameter("Ray", "R", "Ray hits, used for debugging", GH_ParamAccess.tree);
             pManager.AddNumberParameter("Distance", "D", "Distance to each hit, used for debugging", GH_ParamAccess.tree);
             pManager.AddBooleanParameter("Hit", "H", "If you hit the target, used for debugging", GH_ParamAccess.tree);
@@ -50,14 +52,14 @@ namespace GH.MiscToolbox.Components
             List<Mesh> M = new List<Mesh>();
             if (!DA.GetDataList(0, M))
                 return;
-            Mesh Mt = null;
-            if (!DA.GetData(1, ref Mt))
+            List<Mesh> Mt = new List<Mesh>();
+            if (!DA.GetDataList(1, Mt))
                 return;
             List<Point3d> points = new List<Point3d>();
             if (!DA.GetDataList(2, points))
                 return;
-            List<Point3d> targets = new List<Point3d>();
-            if (!DA.GetDataList(3, targets))
+            GH_Structure<GH_Point> targets = new GH_Structure<GH_Point>();
+            if (!DA.GetDataTree(3, out targets))
                 return;
             bool run = false;
             if (!DA.GetData(4, ref run))
@@ -68,61 +70,93 @@ namespace GH.MiscToolbox.Components
                 return;
 
             List<int> hits = new List<int>();
-            var jobs = new List<Tuple<int, int, Ray3d, Mesh, int>>();
+            var jobs = new List<Tuple<int, int, Ray3d, Mesh, int[]>>();
 
             var context = new Mesh();
             M.ForEach(x => context.Append(x));
 
             var faceIndex = context.Faces.Count;
 
-            context.Append(Mt);
-
-            for (int i = 0; i < points.Count; i++)
+            if (Mt.Count != targets.PathCount)
             {
-                for (int j = 0; j < targets.Count; j++)
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Provide equal amount of meshes and targets");
+                return;
+            }
+
+            int[] faceBarriers = new int[Mt.Count];
+            faceBarriers[0] = context.Faces.Count;
+            for (int i = 0; i < Mt.Count - 1; i++)
+            {
+                faceBarriers[i + 1] = Mt[i].Faces.Count;
+            }
+            faceBarriers.Where(x => x == 10).Select((x, i) => i);
+            Mt.ForEach(x => context.Append(x));
+
+            List<List<double>> results = new List<List<double>>();
+            double[][][] distDataResults = new double[Mt.Count][][];
+            bool[][][] intersectDataResults = new bool[Mt.Count][][];
+            Point3d[][][] pointDataResults = new Point3d[Mt.Count][][];
+
+            for (int k = 0; k < Mt.Count; k++)
+            {
+                distData = new double[points.Count][];
+                pointData = new Point3d[points.Count][];
+                intersectData = new bool[points.Count][];
+                targetIndexData = new int[points.Count][];
+
+                for (int i = 0; i < points.Count; i++)
                 {
-                    //  Cast Ray
-                    Ray3d ray = new Ray3d((Point3d)points[i], targets[j] - points[i]);
-                    jobs.Add(new Tuple<int, int, Ray3d, Mesh, int>(i, j, ray, context, faceIndex));
+                    distData[i] = new double[targets[k].Count];
+                    pointData[i] = new Point3d[targets[k].Count];
+                    intersectData[i] = new bool[targets[k].Count];
+                    targetIndexData[i] = new int[targets[k].Count];
                 }
+
+                for (int i = 0; i < points.Count; i++)
+                {
+                    for (int j = 0; j < targets[k].Count; j++)
+                    {
+                        //  Cast Ray
+                        Ray3d ray = new Ray3d(points[i], targets[k][j].Value - points[i]);
+                        jobs.Add(new Tuple<int, int, Ray3d, Mesh, int[]>(i, j, ray, context, faceBarriers));
+                    }
+                }
+
+                System.Threading.Tasks.Parallel.ForEach(jobs, x => RunJob(x));
+                var pointIntersections = intersectData.Select(x => Average(x));
+                distDataResults[k] = distData;
+                intersectDataResults[k] = intersectData;
+                pointDataResults[k] = pointData;
+                results.Merge(pointIntersections.ToList());
             }
 
-            distData = new double[points.Count][];
-            pointData = new Point3d[points.Count][];
-            intersectData = new bool[points.Count][];
-            for (int i = 0; i < points.Count; i++)
+            DA.SetDataTree(0, results.ToTree());
+
+            
+            if (raysDebug)
             {
-                distData[i] = new double[targets.Count];
-                pointData[i] = new Point3d[targets.Count];
-                intersectData[i] = new bool[targets.Count];
+                var lines = pointDataResults.Select((x, i) => x.Select((y, j) => y.Select((z, k) => new Line(points[j], z)).ToArray()).ToArray()).ToArray();
+                DA.SetDataTree(1, lines.ToTree<Line>());
             }
+            
 
-            System.Threading.Tasks.Parallel.ForEach(jobs, x => RunJob(x));
-            var pointIntersections = intersectData.Select(x => Average(x));
-
-            DA.SetDataList(0, pointIntersections);
-
-            if(raysDebug)
+            if (distDebug)
             {
-                var lines = pointData.Select((x, i) => x.Select(y => new Line(points[i], y)).ToArray()).ToArray();
-                DA.SetDataTree(1, lines.ToTree());
+                DA.SetDataTree(2, distDataResults.ToTree<double>());
             }
-
-            if(distDebug)
+            
+            if (hitsDebug)
             {
-                DA.SetDataTree(2, distData.ToTree());
+                DA.SetDataTree(3, intersectDataResults.ToTree<bool>());
             }
-
-            if(hitsDebug)
-            {
-                DA.SetDataTree(3, intersectData.ToTree());
-            }
+            
 
         }
 
         double[][] distData;
         Point3d[][] pointData;
         bool[][] intersectData;
+        int[][] targetIndexData;
         private bool raysDebug = true;
         private bool distDebug = true;
         private bool hitsDebug = true;
@@ -150,6 +184,38 @@ namespace GH.MiscToolbox.Components
                 {
                     if (i >= task.Item5)
                     {
+                        targetHit = true;
+                        break;
+                    }
+                }
+            }
+
+            distData[task.Item1][task.Item2] = d;
+            if (d >= 0)
+                pointData[task.Item1][task.Item2] = task.Item3.PointAt(d);
+            else
+                pointData[task.Item1][task.Item2] = Point3d.Unset;
+
+            intersectData[task.Item1][task.Item2] = d >= 0 && targetHit;
+        }
+
+        public void RunJob(Tuple<int, int, Ray3d, Mesh, int[]> task)
+        {
+            int[] indeces;
+            double d = Rhino.Geometry.Intersect.Intersection.MeshRay(task.Item4, task.Item3, out indeces);
+
+            var targetHit = false;
+            targetIndexData[task.Item1][task.Item2] = -2; // Missed
+            if (indeces != null && indeces.Length > 0)
+            {
+                var index = indeces.First();
+
+                for (int i = 0; i < task.Item5.Length; i++)
+                {
+                    var faceID = task.Item5[i];
+                    if (index <= faceID)
+                    {
+                        targetIndexData[task.Item1][task.Item2] = i - 1;
                         targetHit = true;
                         break;
                     }
--- GH.MiscToolbox/GH.MiscToolbox/GHStructureExtentions.cs
+++ GH.MiscToolbox/GH.MiscToolbox/GHStructureExtentions.cs
@@ -11,15 +11,53 @@ namespace GH.MiscToolbox
 {
     public static class GHStructureExtentions
     {
-        public static DataTree<T> ToTree<T>(this T[][] data)
+        public static DataTree<T> ToTree<T>(this IEnumerable<IEnumerable<T>> data)
         {
             var tree = new DataTree<T>();
-            
-            for (int i = 0; i < data.Length; i++)
+            int i = 0;
+            foreach (var d in data)
             {
-                tree.AddRange(data[i], new GH_Path(i));
+                tree.AddRange(d, new GH_Path(i));
+                i++;
             }
             return tree;
+        }
+
+        public static DataTree<T> ToTree<T>(this IEnumerable<IEnumerable<IEnumerable<T>>> data)
+        {
+            var tree = new DataTree<T>();
+            int j = 0, i = 0;
+
+            foreach (var dd in data)
+            {
+                j = 0;
+                foreach (var d in dd)
+                {
+                    tree.AddRange(d, new GH_Path(i, j));
+                    j++;
+                }
+                i++;
+            }
+            return tree;
+        }
+
+        public static void Merge<T>(this List<List<T>> data, List<T> extra)
+        {
+            if (data.Count == 0)
+            {
+                for (int i = 0; i < extra.Count; i++)
+                {
+                    data.Add(new List<T> { extra[i] });
+                }
+                return;
+            }
+
+            if (data.Count != extra.Count)
+                throw new Exception("Not Same Length");
+            for (int i = 0; i < extra.Count; i++)
+            {
+                data[i].Add(extra[i]);
+            }
         }
     }
 }
